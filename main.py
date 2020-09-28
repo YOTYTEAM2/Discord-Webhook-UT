30b4f1d64591a8240b2f688840d2961865f25919
YOTYTeaM <69961790+YOTYTeaM@users.noreply.github.com>
2020-09-28 02:48:34 -0700

    discord-wh-ut data

---
+++ main.py
@@ -0,0 +1,13 @@
+# Discord Webhook UT --version v1.0
+#Writen by YOTYTeaM, ACAmir
+
+#NEED LIB: /pip
+# discord_webhook
+# emoji
+
+
+from tkinter import *
+from discord_webhook import DiscordWebhook
+from time import *
+from emoji import *
+
